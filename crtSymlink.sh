4a189bcaed4eb98c95f99d2d9aa4284d1fe5da83
Hideo Kawamura <Hideo.90.Kawamura@gmail.com>
2020-12-15 01:05:20 +0900

    change some problem.

--- crtSymlink.sh
+++ crtSymlink.sh
@@ -2,13 +2,13 @@
 
 cd $HOME
 ln -s $HOME/UbuntuDotFiles/.Xdefaults .Xdefaults
-ln -s $HOME/.Xresources .Xresources
-ln -s $HOME/.bash_logout .bash_logout
-ln -s $HOME/.bashrc .bashrc
-ln -s $HOME/.xinitrc .xinitrc
-ln -s $HOME/.profile .profile
-mkdir .emacs.d
-ln -s $HOME/.emacs.d/init.el .emacs.d/init.el
+ln -s $HOME/UbuntuDotFiles/.Xresources .Xresources
+ln -s $HOME/UbuntuDotFiles/.bash_logout .bash_logout
+ln -s $HOME/UbuntuDotFiles/.bashrc .bashrc
+ln -s $HOME/UbuntuDotFiles/.xinitrc .xinitrc
+ln -s $HOME/UbuntuDotFiles/.profile .profile
+mkdir $HOME/.emacs.d
+ln -s $HOME/UbuntuDotFiles/.emacs.d/init.el $HOME/.emacs.d/init.el
 cd $HOME/.emacs.d/
 emacs --batch -f batch-byte-compile init.el
 cd $HOME/.fonts/
