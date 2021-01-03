60c354fefd22e732c20b25152f9ec9218d48f897
Hideo Kawamura <Hideo.90.Kawamura@gmail.com>
2021-01-03 22:25:10 +0900

    change theme add Moody

--- .emacs.d/inits/05_ui.el
+++ .emacs.d/inits/05_ui.el
@@ -8,17 +8,57 @@
 ;;; テーマの設定
 ;;; doom-one-light
 ;;;
-(leaf doom-themes
-  :ensure t treemacs
-  :custom
-  (doom-themes-enable-italic . t)
-  (doom-themes-enable-bold . t)
+;;(leaf doom-themes
+;;  :ensure t treemacs
+;;  :custom
+;;  (doom-themes-enable-italic . t)
+;;  (doom-themes-enable-bold . t)
+;;  :config
+;;  (load-theme 'doom-one-light t)
+;;(load-theme 'doom-one t)
+;;  (doom-themes-visual-bell-config)
+;;(doom-themes-treemacs-config)
+;;(doom-themes-neotree-config)
+;;  (doom-themes-org-config)
+;;  )
+
+;;; Moody(Tabs and ribbons for the mode line)
+
+(leaf moody
+  :doc "Tabs and ribbons for the mode line"
+  :req "emacs-25.3"
+  :tag "emacs>=25.3"
+  :added "2021-01-03"
+  :url "https://github.com/tarsius/moody"
+  :emacs>= 25.3
+  :ensure t
   :config
-  (load-theme 'doom-one-light t)
-  (doom-themes-visual-bell-config)
-  ;;(doom-themes-treemacs-config)
-  ;;(doom-themes-neotree-config)
-  (doom-themes-org-config)
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode))
+
+;;; Modus Themes (Modus Operandi and Modus Vivendi)
+;;; - high contrast theme
+;;; https://protesilaos.com/modus-themes/
+
+(leaf modus-themes
+  :doc "Highly accessible themes (WCAG AAA)"
+  :req "emacs-26.1"
+  :tag "accessibility" "theme" "faces" "emacs>=26.1"
+  :added "2021-01-03"
+  :url "https://gitlab.com/protesilaos/modus-themes"
+  :emacs>= 26.1
+  :ensure t
+  :bind ("<f2>" . modus-themes-toggle)
+  :init
+  (setq modus-themes-slanted-constructs t
+        modus-themes-bold-constructs nil
+        modus-themes-mode-line 'moody)
+  :config
+  ;; Load the theme of your choice
+  (modus-themes-load-operandi)
+  ;; ;; OR
+  ;; (load-theme 'modus-operandi t)
   )
 
 
